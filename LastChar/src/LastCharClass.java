785fc2ffe3b314f4986d972bd72624adde3d38da
James Olas <jamesolas@gmail.com>
2020-12-22 21:37:18 -0800

    sql practice

--- LastChar/src/LastCharClass.java
+++ LastChar/src/LastCharClass.java
@@ -7,18 +7,23 @@ public class LastCharClass {
 		StringBuilder sb = new StringBuilder();
 		StringBuilder rev = new StringBuilder();
 		String ar[] = str.split(" ");
-			for(int i = 0; i < ar.length; i++) {
-				//sb.append(Character.toUpperCase(ar[i].charAt(ar[i].length()-1))).append(ar[i].substring(1)).append(" ");
-				//sb.append(Character.toUpperCase(ar[i].charAt(ar[i].length()-1))).append(String.valueOf(ar[i].charAt(ar[i].length()-1)).substring(1)).append(" ");
-				//sb.append(ar[i].substring(0)).append(Character.toUpperCase(ar[i].charAt(ar[i].length()-1))).append(" ");
-				//sb.append(ar[i].substring(0)).append(" ");
-				rev.append(ar[i]);
-				rev.delete(rev.length()-1,rev.length());
-				char last = Character.toUpperCase(ar[i].charAt(ar[i].length()-1));
-				rev.insert(rev.length(), last).append(" ");
-				
-			}
-				
-				System.out.println(rev.toString().trim());
+		for (int i = 0; i < ar.length; i++) {
+			// sb.append(Character.toUpperCase(ar[i].charAt(ar[i].length()-1))).append(ar[i].substring(1)).append("
+			// ");
+			// sb.append(Character.toUpperCase(ar[i].charAt(ar[i].length()-1))).append(String.valueOf(ar[i].charAt(ar[i].length()-1)).substring(1)).append("
+			// ");
+			// sb.append(ar[i].substring(0)).append(Character.toUpperCase(ar[i].charAt(ar[i].length()-1))).append("
+			// ");
+			// sb.append(ar[i].substring(0)).append(" ");
+//				rev.append(ar[i]);
+//				rev.delete(rev.length()-1,rev.length());
+//				char last = Character.toUpperCase(ar[i].charAt(ar[i].length()-1));
+//				rev.insert(rev.length(), last).append(" ");
+
+		sb.append(ar[i].substring(0, ar[i].length()-1)).append(Character.toUpperCase(ar[i].charAt(ar[i].length()-1))).append(" ");
+		}
+		System.out.println(sb.toString().trim());
+
+		//System.out.println(rev.toString().trim());
 	}
 }
